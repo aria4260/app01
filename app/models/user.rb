38c8457434e6eb1fb2cafa663b1303a90435a0f4
aria4260 <aria4260@gmail.com>
2018-05-02 06:47:46 +0000

    Update migration 2

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ApplicationRecord
   has_many :posts, dependent: :destroy
-  has_many :follows, dependent: :destroy
+  has_many :active_relationships, class_name: "Follow",
+                                  foreign_key: "user_id",
+                                  dependent: :destroy
+  has_many :passive_relationships, class_name: "Follow",
+                                   foreign_key: "follow_user_id",
+                                   dependent: :destroy
+  has_many :following, through: :active_relationships, source: :follow_user
+  has_many :followers, through: :passive_relationships, source: :user                  
   has_many :likes
   has_many :comments
 end
